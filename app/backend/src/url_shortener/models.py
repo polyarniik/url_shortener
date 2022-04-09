22908dcaca7a8cb256e2e9be6b40f43570266347
polyarniik <rus.safiullin.01@mail.ru>
2022-04-09 16:27:56 +0300

    + ShorterLink model

--- app/backend/src/url_shortener/models.py
+++ app/backend/src/url_shortener/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from url_shortener.utils import shorter
+
+
+class ShortLink(models.Model):
+    link = models.URLField(verbose_name='link')
+    short_link = models.URLField(verbose_name='short_link')
+
+    def get_shorter_link(self):
+        short = shorter(self.link)
+        while ShortLink.objects.filter(short_link=short).exists():
+            short = shorter(self.link)
+
+        return short
+
+    def __str__(self):
+        return self.link
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.short_link = self.get_shorter_link()
+        return super().save(*args, **kwargs)
