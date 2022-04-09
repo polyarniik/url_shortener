56d182a12ff6c51ea4ca8d298d0e380ac6f86745
polyarniik <rus.safiullin.01@mail.ru>
2022-04-09 16:29:34 +0300

    + views for create short url and for redirect to correct page

--- app/backend/src/url_shortener/views.py
+++ app/backend/src/url_shortener/views.py
@@ -1,3 +1,33 @@
+from django.http import HttpResponseRedirect, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+from url_shortener.forms import LinkForm
+from url_shortener.models import ShortLink
+
+
+def main_page_view(request):
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            link = ShortLink(
+                link=form.cleaned_data['link']
+            )
+            link.save()
+            return render(request, "url_shortener/main.html", {
+                'form': form,
+                'short_link': str('http://' + request.get_host()) + '/' + str(link.short_link)
+            })
+
+    if request.method == 'GET':
+        form = LinkForm()
+        return render(request, 'url_shortener/main.html', {
+            'form': form,
+        })
+
+
+def redirect_view(request, short):
+    short_link = ShortLink.objects.filter(short_link=short)
+    if short_link is not None:
+        return HttpResponseRedirect(short_link[0].link)
+    else:
+        return HttpResponseNotFound('<h1> Link not found <h1>')
