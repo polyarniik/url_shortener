7443aca75ca6ac370107e4dd80a268a878b24c42
polyarniik <rus.safiullin.01@mail.ru>
2022-05-20 00:20:34 +0300

    + salt to shorter algorithm

--- app/backend/src/url_shortener/utils.py
+++ app/backend/src/url_shortener/utils.py
@@ -1,4 +1,6 @@
+import binascii
 import hashlib
+import os
 import random
 
 
@@ -8,11 +10,10 @@ def shorter(link):
     :param link:
     :return:
     """
-    link_hash = hashlib.md5(str(link[0]).encode("UTF-8"))
+    salt = binascii.hexlify(os.urandom(5)).decode()
+    link += salt
+    link_hash = hashlib.md5(link.encode("UTF-8")).hexdigest()
     short_link = ""
-    for i in range(0, random.randrange(4, 7)):
-        short_link += link_hash.hexdigest()[
-            random.randrange(len(link_hash.hexdigest()))
-        ]
-    print(short_link)
+    for i in range(8):
+        short_link += link_hash[random.randint(0, len(link_hash) - 1)]
     return short_link
